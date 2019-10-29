cdbef321a823316a0197365e96bb7978342751a9
bgwtz4 <b.winston87@yahoo.com>
2019-10-28 22:16:54 -0500

    Tic tac toe board created and array for (x's) position.

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String [] args) {
+
+        char[] pos = {' ',' ',' ',' ',' ',' ',' ',' '};
+        System.out.println("    |    |    ");
+        System.out.println("  " + pos[0]  +" |  "+ pos[1] + " |   ");
+        System.out.println("    |    |    ");
+        System.out.println("----+----+----");
+        System.out.println("  " + pos[0]  +" |  "+ pos[1] + " |   ");
+
+    }
+}
